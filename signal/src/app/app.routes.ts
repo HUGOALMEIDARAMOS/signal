37736feed16d24ba5e899bcc10c07729b73bb98b
hugoalmeidaramos <hugo.undb@gmail.com>
2024-06-17 12:37:20 -0300

    ✨ Configuração do arquivo de rotas

--- signal/src/app/app.routes.ts
+++ signal/src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', loadComponent: () => import('../tasks/new-task/new-task.component').then( c => c.NewTaskComponent)}
+];
